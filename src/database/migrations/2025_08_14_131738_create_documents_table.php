fbc5fe2b839f7dd695edaecd2073fb5fe9fb1c31
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-08-15 19:49:08 +0900

    documentsとfoldersテーブル作成

---
+++ src/database/migrations/2025_08_14_131738_create_documents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('folder_id')->nullable()->comment('フォルダID');
+            $table->string('title')->comment('タイトル');
+            $table->text('content')->nullable()->comment('文書内容');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
